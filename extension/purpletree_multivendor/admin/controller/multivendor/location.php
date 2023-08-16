a39782cab0a41f41bce2fe5d046debea0a3b33f2
joshanachauhan <128023132+joshanachauhan@users.noreply.github.com>
2023-08-16 11:51:03 +0530

    location twig

---
+++ extension/purpletree_multivendor/admin/controller/multivendor/location.php
@@ -0,0 +1,371 @@
+<?php
+namespace Opencart\Admin\Controller\Extension\PurpletreeMultivendor\Multivendor;
+class Location extends \Opencart\System\Engine\Controller {
+	private $error = array();	
+	public function index() {	
+			$this->load->language('extension/purpletree_multivendor/multivendor/location');
+			
+			$this->document->setTitle($this->language->get('heading_title'));
+			
+			$this->load->model('extension/purpletree_multivendor/multivendor/location');
+			
+			$this->getList();
+	}
+		
+	public function add() {
+			
+			$this->load->language('extension/purpletree_multivendor/multivendor/location');
+			$this->document->setTitle($this->language->get('heading_title'));
+			
+			$this->load->model('extension/purpletree_multivendor/multivendor/location');
+			
+			if (($this->request->server['REQUEST_METHOD'] == 'POST')  /*&& $this->validateForm()*/ ) {
+				
+				$this->model_extension_purpletree_multivendor_multivendor_location->addSeatingmanagement($this->request->post);
+				
+				$this->session->data['success'] = $this->language->get('text_success');
+				
+				$url = '';
+				
+				if (isset($this->request->get['sort'])) {
+					$url .= '&sort=' . $this->request->get['sort'];
+				}
+				
+				if (isset($this->request->get['order'])) {
+					$url .= '&order=' . $this->request->get['order'];
+				}
+				
+				if (isset($this->request->get['page'])) {
+					$url .= '&page=' . $this->request->get['page'];
+				}
+				
+				$this->response->redirect($this->url->link('extension/purpletree_multivendor/multivendor/location', 'user_token=' . $this->session->data['user_token'] . $url, true));
+			} 
+			$this->getForm();
+	}
+		
+	public function edit() {
+			
+			$this->load->language('extension/purpletree_multivendor/multivendor/location');
+			
+			$this->document->setTitle($this->language->get('heading_title'));
+			
+			$this->load->model('extension/purpletree_multivendor/multivendor/location');
+			
+			if (($this->request->server['REQUEST_METHOD'] == 'POST')  && $this->validateForm() ) {				
+				$this->model_extension_purpletree_multivendor_multivendor_location->editSeatingmanagement($this->request->get['table_id'], $this->request->post);
+				
+				$this->session->data['success'] = $this->language->get('text_success');
+				
+				$url = '';
+				
+				if (isset($this->request->get['sort'])) {
+					$url .= '&sort=' . $this->request->get['sort'];
+				}
+				
+				if (isset($this->request->get['order'])) {
+					$url .= '&order=' . $this->request->get['order'];
+				}
+				
+				if (isset($this->request->get['page'])) {
+					$url .= '&page=' . $this->request->get['page'];
+				}
+				
+				$this->response->redirect($this->url->link('extension/purpletree_multivendor/multivendor/location', 'user_token=' . $this->session->data['user_token'] . $url, true));
+			}
+			
+			$this->getForm();
+	}
+		
+	public function delete() {
+			
+			$this->load->language('extension/purpletree_multivendor/multivendor/location');
+			
+			$this->document->setTitle($this->language->get('heading_title'));
+			
+			$this->load->model('extension/purpletree_multivendor/multivendor/location');
+			
+			if (isset($this->request->post['selected']) && $this->validateDelete()) {
+				
+				$url = '';
+				
+				foreach ($this->request->post['selected'] as $table_id) {
+					$this->model_extension_purpletree_multivendor_multivendor_location->deleteSeatingManagement($table_id);		
+							
+					$this->session->data['success'] = $this->language->get('text_success');
+						
+					
+				}
+				
+				if (isset($this->request->get['sort'])) {
+					$url .= '&sort=' . $this->request->get['sort'];
+				}
+				
+				if (isset($this->request->get['order'])) {
+					$url .= '&order=' . $this->request->get['order'];
+				}
+				
+				if (isset($this->request->get['page'])) {
+					$url .= '&page=' . $this->request->get['page'];
+				}
+				
+				$this->response->redirect($this->url->link('extension/purpletree_multivendor/multivendor/location', 'user_token=' . $this->session->data['user_token'] . $url, true));
+			}
+			
+			$this->getList();
+	}
+	protected function getList() {
+			
+			$url = '';
+			
+			if (isset($this->request->get['sort'])) {
+			$url .= '&sort=' . $this->request->get['sort'];
+			}
+
+			if (isset($this->request->get['order'])) {
+				$url .= '&order=' . $this->request->get['order'];
+			}
+
+			if (isset($this->request->get['page'])) {
+				$url .= '&page=' . $this->request->get['page'];
+			}
+			
+			$data['breadcrumbs'] = array();
+			
+			/*$filter_data = array(
+			'sort'  => $sort,
+			'order' => $order,
+			'start' => ($page - 1) * $this->config->get('config_pagination_admin'),
+			'limit' => $this->config->get('config_pagination_admin')
+			);*/
+			
+			$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_home'),
+			'href' => $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'], true)
+			);
+			
+			$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('heading_title'),
+			'href' => $this->url->link('extension/purpletree_multivendor/multivendor/location', 'user_token=' . $this->session->data['user_token'] . $url, true)
+			);
+			
+			$data['add'] = $this->url->link('extension/purpletree_multivendor/multivendor/location|add', 'user_token=' . $this->session->data['user_token'] . $url, true);
+			$data['delete'] = $this->url->link('extension/purpletree_multivendor/multivendor/location|delete', 'user_token=' . $this->session->data['user_token'] . $url, true);
+			
+			$data['seatingmanagements'] = array();
+			
+			$seatingmanagement_total = $this->model_extension_purpletree_multivendor_multivendor_location->getTotalSeatingManagements();
+			
+			$results = $this->model_extension_purpletree_multivendor_multivendor_location->getSeatingManagements();
+			$text_enabled =  $this->language->get('text_enabled');
+			$text_disabled =  $this->language->get('text_disabled');
+			foreach ($results as $result) {	
+				$data['seatingmanagements'][] = array(
+				'table_id'        => $result['table_id'],
+				'seat_capacity' => $result['seat_capacity'],
+				'table_no'        => $result['table_no'],
+				'status'       	   => ($result['status'])? $text_enabled :$text_disabled,
+				'vendor_store_id'    => $result['vendor_store_id'],				
+				'vendor_id'    => $result['vendor_id'],				
+				'edit'        => $this->url->link('extension/purpletree_multivendor/multivendor/location|edit', 'user_token=' . $this->session->data['user_token'] . '&table_id=' . $result['table_id'] . $url, true),
+				'delete'      => $this->url->link('extension/purpletree_multivendor/multivendor/location|delete', 'user_token=' . $this->session->data['user_token'] . '&table_id=' . $result['table_id'] . $url, true)
+				);
+			} 
+			
+			
+			
+			if (isset($this->error['warning'])) {
+				$data['error_warning'] = $this->error['warning'];
+				}elseif (isset($this->session->data['error_warning'])) {
+				$data['error_warning'] = $this->session->data['error_warning'];
+				unset($this->session->data['error_warning']);
+				} else {
+				$data['error_warning'] = '';
+			}
+			
+			
+			if (isset($this->session->data['success'])) {
+				$data['success'] = $this->session->data['success'];
+				
+				unset($this->session->data['success']);
+				} else {
+				$data['success'] = '';
+			}
+			
+			if (isset($this->request->post['selected'])) {
+				$data['selected'] = (array)$this->request->post['selected'];
+				} else {
+				$data['selected'] = array();
+			}
+			
+			/*$data['pagination'] = $this->load->controller('common/pagination', [
+			'total' => $seatingmanagement_total,
+			'page'  => $page,
+			'limit' => $this->config->get('config_pagination_admin'),
+			'url'   => $this->url->link('extension/purpletree_multivendor/multivendor/location', '' . $url . '&page={page}'.'language=' . $this->config->get('config_language'), true)
+		]);
+
+			$data['results'] = sprintf($this->language->get('text_pagination'), ($seatingmanagement_total) ? (($page - 1) * $this->config->get('config_pagination_admin')) + 1 : 0, ((($page - 1) * $this->config->get('config_pagination_admin')) > ($seatingmanagement_total - $this->config->get('config_pagination_admin'))) ? $seatingmanagement_total : ((($page - 1) * $this->config->get('config_pagination_admin')) + $this->config->get('config_pagination_admin')), $seatingmanagement_total, ceil($seatingmanagement_total / $this->config->get('config_pagination_admin')));
+			
+			$data['sort'] = $sort;
+			$data['order'] = $order;*/
+			
+			$data['header'] = $this->load->controller('common/header');
+			$data['column_left'] = $this->load->controller('common/column_left');
+			$data['footer'] = $this->load->controller('common/footer');
+			
+			$this->response->setOutput($this->load->view('extension/purpletree_multivendor/multivendor/location', $data));
+		}
+		
+		protected function getForm() {
+			$data['text_form'] = !isset($this->request->get['table_id']) ? $this->language->get('text_add') : $this->language->get('text_edit');
+			
+			if (isset($this->error['warning'])) {
+				$data['error_warning'] = $this->error['warning'];
+				} else {
+				$data['error_warning'] = '';
+			}
+			
+			
+			if (isset($this->error['name'])) {
+				$data['error_name'] = $this->error['name'];
+				} else {
+				$data['error_name'] = array();
+			}
+			
+			
+			
+			if (isset($this->error['sort_order'])) {
+				$data['error_sort_order'] = $this->error['sort_order'];
+				} else {
+				$data['error_sort_order'] = '';
+			}
+			$url = '';
+			
+			if (isset($this->request->get['sort'])) {
+				$url .= '&sort=' . $this->request->get['sort'];
+			}
+			
+			if (isset($this->request->get['order'])) {
+				$url .= '&order=' . $this->request->get['order'];
+			}
+			
+			if (isset($this->request->get['page'])) {
+				$url .= '&page=' . $this->request->get['page'];
+			}
+			
+			$data['breadcrumbs'] = array();
+			
+			$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_home'),
+			'href' => $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'], true)
+			);
+			
+			$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('heading_title'),
+			'href' => $this->url->link('extension/purpletree_multivendor/multivendor/location', 'user_token=' . $this->session->data['user_token'] . $url, true)
+			);
+			$this->load->language('extension/purpletree_multivendor/multivendor/location');
+			$data['entry_joinning_fee']=$this->language->get('entry_joinning_fee');
+			$data['entry_default_subscription_plan']=$this->language->get('entry_default_subscription_plan');
+			
+			$data['cancel'] = $this->url->link('extension/purpletree_multivendor/multivendor/location', 'user_token=' . $this->session->data['user_token'] . $url, true);
+			
+			
+			$data['user_token'] = $this->session->data['user_token'];
+			
+			$this->load->model('localisation/language');
+			
+			$data['languages'] = $this->model_localisation_language->getLanguages();
+			
+			$this->load->model('setting/store');
+
+			$data['stores'] = [];
+
+			$data['stores'][] = [
+				'store_id' => 0,
+				'name'     => $this->language->get('text_default')
+			];
+
+			$stores = $this->model_setting_store->getStores();
+			foreach ($stores as $store) {
+				$data['stores'][] = [
+					'store_id' => $store['store_id'],
+					'name'     => $store['name']
+				];
+			}
+			
+			$this->load->model('extension/purpletree_multivendor/multivendor/vendor');
+			
+			$data['sellers'] = $this->model_extension_purpletree_multivendor_multivendor_vendor->getVendors();
+			
+			if (isset($this->request->get['table_id']) && ($this->request->server['REQUEST_METHOD'] != 'POST')) {
+				$seating_management_info = $this->model_extension_purpletree_multivendor_multivendor_location->getSeatingManagement($this->request->get['table_id']);		
+			}
+			//print_r($seating_management_info);exit;
+			
+			if (isset($this->request->post['status'])) {
+				$data['status'] = $this->request->post['status'];
+				} elseif (!empty($seating_management_info)) {
+				$data['status'] = $seating_management_info['status'];
+				} else {
+				$data['status'] = '';
+			}
+			
+			if (isset($this->request->post['table_no'])) {
+				$data['table_no'] = $this->request->post['table_no'];
+				} elseif (!empty($seating_management_info)) {
+				$data['table_no'] = $seating_management_info['table_no'];
+				} else {
+				$data['table_no'] = '';
+			}
+			if (isset($this->request->post['vendor_id'])) {
+				$data['vendor_id'] = $this->request->post['vendor_id'];
+				} elseif (!empty($seating_management_info)) {
+				$data['vendor_id'] = $seating_management_info['vendor_id'];
+				} else {
+				$data['vendor_id'] = '';
+			}
+			
+			if (isset($this->request->post['seat_capacity'])) {
+				$data['seat_capacity'] = $this->request->post['seat_capacity'];
+				} elseif (!empty($seating_management_info)) {
+				$data['seat_capacity'] = $seating_management_info['seat_capacity'];
+				} else {
+				$data['seat_capacity'] = '';
+			}
+			
+			if (isset($this->request->post['vendor_store_id'])) {
+				$data['vendor_store_id'] = $this->request->post['vendor_store_id'];
+				} elseif (!empty($seating_management_info)) {
+				$data['vendor_store_id'] = $seating_management_info['vendor_store_id'];
+				} else {
+				$data['vendor_store_id'] = 0;
+			}
+
+			$data['header'] = $this->load->controller('common/header');
+			$data['column_left'] = $this->load->controller('common/column_left');
+			$data['footer'] = $this->load->controller('common/footer');
+			
+			$this->response->setOutput($this->load->view('extension/purpletree_multivendor/multivendor/location', $data));
+		}
+		
+		protected function validateForm() {
+			foreach ($this->request->post['table_no'] as $language_id => $value) {
+				if ((strlen($value['name']) < 1) || (strlen($value['name']) > 255)) {
+					$this->error['name'][$language_id] = $this->language->get('error_name');
+				}
+				
+			}
+			return !$this->error;
+		}
+		
+		protected function validateDelete() {
+			if (!$this->user->hasPermission('modify', 'extension/purpletree_multivendor/multivendor/location')) {
+				$this->error['warning'] = $this->language->get('error_permission');
+			}
+			
+			return !$this->error;
+		}
+}
+?>
---
+++ extension/purpletree_multivendor/admin/language/en-gb/multivendor/location.php
@@ -0,0 +1,33 @@
+<?php
+// Heading
+$_['heading_title']      = 'Location';
+
+// Text
+$_['text_success']       = 'Success: You have modified location!';
+$_['text_list']          = 'Location List';
+$_['text_add']           = 'Add Location';
+$_['text_edit']          = 'Edit Location';
+$_['text_geo_zone']      = 'Location';
+
+// Column
+$_['column_vendor']        = 'Vendor';
+$_['column_name']        = 'Name';
+$_['column_sort_order'] = 'Sort Order';
+$_['column_action']      = 'Action';
+$_['column_status']      = 'Status';
+
+// Entry
+$_['entry_name']         = 'Location Name';
+$_['entry_sort_order']  = 'Sort Order';
+$_['entry_status']  = 'Status';
+$_['entry_vendor']  = 'Vendor';
+$_['entry_location']  = 'Location';
+
+// Error
+$_['error_permission']   = 'Warning: You do not have permission to modify location!';
+$_['error_name']         = 'Seating Management Name must be between 3 and 32 characters!';
+$_['error_store_name']   = 'Store name must be between 3 and 32 characters!';
+$_['error_price_numeric']   = 'Price must be in number!';
+$_['error_price']   = 'Price must be between 1 and 8 numbers!';
+$_['error_description']  = 'Description Name must be between 3 and 255 characters!';
+$_['error_tax_rate']     = 'Warning: This geo zone cannot be deleted as it is currently assigned to one or more tax rates!';
